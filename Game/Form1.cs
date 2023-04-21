9d812e0fd0bf72f6ca5b0936a0734587bb43870f
nima1991zare <93324739+nima1991zare@users.noreply.github.com>
2023-04-22 00:43:04 +0430

    nima

--- Game/Form1.cs
+++ Game/Form1.cs
@@ -13,6 +13,11 @@ namespace Game
     public partial class Form1 : Form
     {
         int rnd = 0, counter = 0;
+        int minnumber = 100;
+        int maxnumber = 999;
+        int input;
+
+
         public Form1()
         {
             InitializeComponent();
@@ -20,40 +25,7 @@ namespace Game
 
         private void button1_Click(object sender, EventArgs e)
         {
-            try
-            {
-
-                int input = int.Parse(textBox1.Text);
-                textBox1.Text = "";
-                if (input > 999 || input < 100)
-                {
-                    MessageBox.Show("Number is not in range");
-                    return;
-                }
-                counter++;
-                label2.Text = "Round: " + counter.ToString();
-                if (counter > 10)
-                {
-                    MessageBox.Show("Game Over!");
-                    button1.Enabled = false;
-                    return;
-                }
-                listBox1.Items.Add(input);
-                if (input > rnd)
-                    MessageBox.Show("Your Guess > this Number");
-                else if (input < rnd)
-                    MessageBox.Show("Your Guess < this Number");
-                else
-                {
-                    MessageBox.Show("WIN");
-                    label2.Text += " - You WIN";
-                    button1.Enabled = false;
-                }
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message);
-            }
+           
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -62,6 +34,62 @@ namespace Game
             rnd = r.Next(100,999);
             label1.Text = rnd.ToString();
             label2.Text = "Round: " + counter.ToString();
+
+
+
+
+            try
+            {
+
+                do
+                {
+                    input = (minnumber + maxnumber) / 2;
+
+                    textBox1.Text=input.ToString();
+                    button1.PerformClick();
+                    textBox1.Text = "";
+
+
+                    if (input > 999 || input < 100)
+                    {
+                        MessageBox.Show("Number is not in range");
+                        return;
+                    }
+                    counter++;
+                    label2.Text = "Round: " + counter.ToString();
+                    if (counter > 10)
+                    {
+                        MessageBox.Show("Game Over!");
+                        button1.Enabled = false;
+                        return;
+                    }
+                    listBox1.Items.Add(input);
+                    if (input > rnd)
+                    {
+                        MessageBox.Show("Your Guess > this Number");
+                        maxnumber = input + 1;
+                    }
+                    else if (input < rnd)
+                    {
+                        MessageBox.Show("Your Guess < this Number");
+                        minnumber = input + 1;
+                    }
+                    else
+                    {
+                        MessageBox.Show("WIN");
+                        label2.Text += " - You WIN";
+                        button1.Enabled = false;
+                    }
+                } while (input != rnd);
+
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
+
+
     }
 }
